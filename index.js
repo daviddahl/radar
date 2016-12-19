072091e35c8982780be4d4f57e2c12faf4f84d6c
David Dahl <ddahl@nulltxt.se>
2016-12-18 20:52:46 -0500

    simplified the watch event handler to not trip over others that might be running at the same time

--- index.js
+++ index.js
@@ -9,66 +9,84 @@ let Gpio = require('onoff').Gpio,
     authToken = process.env.RADAR_TWILIO_AUTHTOKEN,
     mediaUrl = process.env.RADAR_MEDIA_URL,
     //require the Twilio module and create a REST client
-    client = require('twilio')(accountSid, authToken);
+    client = require('twilio')(accountSid, authToken),
+    uuidV4 = require('uuid/v4');
 
-let sendSMS = true;
-let smsCounter = 0;
-let ARMED = false;
+function motionWatcher (err, value) {
+    let fileName;
 
-let lastPhotoTime = null;
-let fileName;
-
-// XXX: Log everything to web api. Cloudant?
-pir.watch(function (err, value) {
     if (err) {
-	console.error(err);
-	throw err;
+        console.error(err);
+        // SEND SMS
+        notifyFault(err);
+        throw err;
     }
-    debugger;
-    // XXX: We have a motion event. Take a picture.
-    if (value === 1) {
-	console.info(`Motion detected... ${value}`);
-	if (!lastPhotoTime || (lastPhotoTime < Date.now() - 60)) {
-	    // Take picture
-	    var camera = new RaspiCam({
-		mode: "photo",
-		output: "/var/www/html/pi-photos/image-%06d.jpg",
-		encoding: "jpg",
-		timeout: 0 // take the picture immediately
-	    });
-	    
-	    camera.on("start", function( err, timestamp ){
-		console.log("photo started at " + timestamp );
-		lastPhotoTime = Date.now();
-	    });
-	    
-	    camera.on("read", function( err, timestamp, filename ){
-		console.log("photo image captured with filename: " + filename );
-		fileName = filename
-		notify();
-	    });
-	    
-	    camera.on("exit", function( timestamp ){
-		console.log("photo child process has exited at " + timestamp );
-	    });
 
-	    function notify () {
-		client.messages.post({
-		    to: TO_PHONE_NUMBER,
-		    from: FROM_PHONE_NUMBER,
-		    body: `Motion detected at ${new Date()}`,
-		    mediaUrl: `${mediaUrl}${fileName}`
-		}, function (err, message) {
-		    if (err) {
-			return console.error('Cannot send SMS', err);
-		    }
-		    console.log(message);
-		});
-	    }
-	    camera.start();
-	}
-    }
-});
+    pir.unwatch();
+
+    console.info(`Motion detected... ${value}`);
+
+    var camera = new RaspiCam({
+        mode: "photo",
+        output: `/var/www/html/pi-photos/image-${uuidV4()}.jpg`,
+        encoding: "jpg",
+        timeout: 2,
+        w: 640,
+        h: 480
+    });
+
+    camera.on("start", function( err, timestamp ){
+        console.log("photo started at " + timestamp );
+    });
+
+    camera.on("read", function( err, timestamp, filename ) {
+        console.log("photo image captured with filename: " + filename );
+        if (filename.indexOf('~') > -1) {
+            // Fucking rename operation hoses the MMS file capture!
+            return;
+        }
+        fileName = filename;
+        notify(fileName);
+    });
+
+    camera.on("exit", function( timestamp ){
+        console.log("photo child process has exited at " + timestamp );
+    });
+    camera.start();
+}
+
+function notify (fileName) {
+    debugger;
+    client.messages.post({
+        to: TO_PHONE_NUMBER,
+        from: FROM_PHONE_NUMBER,
+        body: `Motion detected @ ${new Date()}\n${mediaUrl}${fileName}`,
+        // mediaUrl: `${mediaUrl}${fileName}`
+    }, function (err, message) {
+        if (err) {
+            return console.error('Cannot send SMS', err);
+            pir.watch(motionWatcher);
+        }
+        console.log(message);
+        pir.watch(motionWatcher);
+    });
+}
+
+function notifyFault (smsMessage) {
+    client.messages.post({
+        to: TO_PHONE_NUMBER,
+        from: FROM_PHONE_NUMBER,
+        body: `RADAR ERROR at ${new Date()}: ${smsMessage}`
+    }, function (err, message) {
+        if (err) {
+            return console.error('Cannot send fault SMS', err);
+        }
+        console.log(message);
+        pir.watch(motionWatcher);
+    });
+}
+
+pir.watch(motionWatcher);
 
 process.on('SIGINT', function () {
     pir.unexport();
