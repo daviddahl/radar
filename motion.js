99ce75f0d0d2b077e444bff00cfe2af4f60997ce
David Dahl <ddahl@nulltxt.se>
2017-02-16 23:22:41 -0500

    fixed up status.js so it quits cleanly

--- motion.js
+++ motion.js
@@ -2,9 +2,14 @@ let Gpio = require('onoff').Gpio,
     pir = new Gpio(4, 'in', 'both'),
     uuidV4 = require('uuid/v4'),
     _redis = require('redis'),
+    moment = require('moment-timezone'),
     RADAR_MOTION_LOCATION = process.env.RADAR_MOTION_LOCATION,
+    RADAR_TZ = process.env.RADAR_TZ,
+    RADAR_BEGIN_AUTO_ON_TIME = process.env.RADAR_BEGIN_AUTO_ON_TIME,
+    RADAR_BEGIN_AUTO_OFF_TIME = process.env.RADAR_BEGIN_AUTO_OFF_TIME,
     getLightSwitch = require('./wemo'),
-    systemStatus = 1;
+    systemStatus = 1,
+    systemStatusOverride = true;
 
 
 let redis = {
@@ -12,38 +17,50 @@ let redis = {
     recordMotion: function recordMotion (motionRecord) {
         this.client.publish('motion-channel', motionRecord);
     },
-    checkStatus: function checkStatus () {
+    checkStatus: function checkStatus (callback) {
 	this.client.get('system-status', function (err, reply) {
 	    if (err) {
 		return console.error(err);
 	    }
 	    if (!isNaN(parseInt(reply))) {
 		systemStatus = parseInt(reply);
+		callback(systemStatus);
 	    }
 	});
     }
 };
 
 function motionWatcher (err, value) {
-    redis.checkStatus(); // XXX: add the motionWatcher code into the checkStatus callback
-    if (!checkStatus) {
-	return;
+    // Check if someone is home or it is > 22:00 < 7:00
+    let currentTime = moment().tz(RADAR_TZ).format().split("T")[1].split(":");
+    // [ '17', '05', '11-05', '00' ]
+    let hour = parseInt(currentTime[0]);
+    if (hour > parseInt(RADAR_BEGIN_AUTO_ON_TIME) || hour < parseInt(RADAR_BEGIN_AUTO_OFF_TIME)) {
+	// override status
+	systemStatusOverride = true;
+    } else {
+	systemStatusOverride = false;
     }
-    if (!value) {
-        return console.info('PIR reset');
-    }
-    getLightSwitch('on');
-    pir.unwatch();
-    // Tell redis we have observed motion
-    let uuid = uuidV4();
-    let record = `${new Date()}|${Date.now()}|Motion observed in ${RADAR_MOTION_LOCATION}|${uuid}`;
-    redis.recordMotion(record);
-    console.info('motion recorded');
-    pir.watch(motionWatcher);
+    redis.checkStatus(function (status) {
+	if (!status && !systemStatusOverride) {
+	    console.info(`${Date.now()}: No motion detected`);
+	    return;
+	}
+	if (!value) {
+            return console.info('PIR reset');
+	}
+	getLightSwitch('on');
+	pir.unwatch();
+	// Tell redis we have observed motion
+	let uuid = uuidV4();
+	let record = `${new Date()}|${Date.now()}|Motion observed in ${RADAR_MOTION_LOCATION}|${uuid}`;
+	console.info('motion recorded: ', record);
+	redis.recordMotion(record);
+	pir.watch(motionWatcher);
+    });
 }
 
 pir.watch(motionWatcher);
-redis.checkStatus();
 
 process.on('SIGINT', function () {
     pir.unexport();
--- status.js
+++ status.js
@@ -1,42 +1,75 @@
 #! /usr/bin/env node
-var yargs = require('yargs').argv,
-    _redis = require('redis'),
+var _redis = require('redis'),
     knownMacs = process.env.RADAR_MAC_ADDRESSES;
 
 var redis = {
     client: null,
     setStatus: function setStatus (turnOn) {
-        this.client = _redis.createClient()
 	this.client.set('system-status', turnOn);
-	this.client.quit();
 	return;
     }
 };
 
-redis.client;
+if (process.argv.length) {
+    console.info('ARGV: \n', process.argv);
+    let possibleMACS = [];
 
-if (yargs._[0]) {
-    if(parseTable(yargs._[0]) > -1) {
-	return updateStatus(true);
+    for (let i = 0; i < process.argv.length; i++) {
+	if (process.argv[i].indexOf(':') > -1) {
+	    possibleMACS.push(process.argv[i]);
+	}
     }
-    updateStatus(false);
+
+    let macsToCheck = possibleMACS.join(" ");
+    console.info(macsToCheck);
+    redis.client = _redis.createClient();
+    // XXX: validate arp table data a bit more
+    storeArpTable(macsToCheck, function (err, reply) {
+	if (err) {
+	    redis.client.quit();
+	    console.error(err);
+	    process.exit();
+	}
+	// Check the data for MACS
+	redis.client.get('arpData', function (err, reply) {
+	    if (err) {
+		redis.client.quit();
+		console.error(err);
+		process.exit();
+	    }
+	    if (parseTable(reply)) {
+		// we see a MAC
+		redis.setStatus(0);
+	    } else {
+		redis.setStatus(1);
+	    }
+	    redis.client.quit();
+	    process.exit();
+	});
+    });
 } else {
-    console.error('No MACS');
+    console.error('No args?!');
+    redis.client.quit();
     process.exit();
 }
 
-function parseTable (arpTable) {
-    console.log('knownmacs? ', arpTable.indexOf(knownMacs));
-    return arpTable.indexOf(knownMacs);
-}
-
-function updateStatus (turnOn) {
-    console.log('call redis, set on or off');
-    debugger;
-    if (turnOn) {
-        return redis.setStatus(1);
-    } else {
-	return redis.setStatus(0);
+function storeArpTable (arpTableData, callback) {
+    if (!arpTableData) {
+	return callback('No arp data available');
     }
-    process.exit();
+    redis.client.set('arpData', arpTableData, callback);
 }
+
+function parseTable (arpTableData) {
+    console.log('knownmacs? ', arpTableData.indexOf(knownMacs));
+    return arpTableData.indexOf(knownMacs);
+}
+
+process.on('SIGINT', function () {
+    try {
+	redis.client.quit();
+	process.exit();
+    } catch (ex) {
+	process.exit();
+    }
+});
